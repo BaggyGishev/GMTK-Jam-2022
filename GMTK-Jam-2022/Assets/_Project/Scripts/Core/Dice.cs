c8a76758b99642c6616aa119cae22b9715efeac5
Bohdan Tsaplia <b.tsaplia@gmail.com>
2022-07-16 16:44:10 +0300

    add dice

---
+++ GMTK-Jam-2022/Assets/_Project/Scripts/Core/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Gisha.GMTK2022.Core
+{
+    public class Dice : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] _sideSprites = new Sprite[6];
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(DiceRoutine());
+        }
+
+        private IEnumerator DiceRoutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1f);
+                _spriteRenderer.sprite = _sideSprites[Random.Range(0, _sideSprites.Length)];
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.collider.CompareTag("Player"))
+                Debug.Log("Roll of the dice!");
+        }
+    }
+}
